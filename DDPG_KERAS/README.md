d21469b432e643d28f331001832d7bdb114c3d39
mapestar@gmail.com <hsyoon94>
2020-09-20 16:35:04 +0900

    baseline added

---
+++ DDPG_KERAS/README.md
@@ -0,0 +1,17 @@
+# keras-ddpg
+Python keras + tensorflow implementation of DDPG solving modified open gymAI pendulum-v0 environment
+
+Corrected and modified implementation from [Reinforcement Learning w/ Keras + OpenAI: Actor-Critic Models](https://towardsdatascience.com/reinforcement-learning-w-keras-openai-actor-critic-models-f084612cfd69)
+of DDPG using keras + tensorflow. 
+
+It solves open gymAI pendulum-v0 environment
+
+About METHOD:
+[Continuous control with deep reinforcement learning](https://arxiv.org/abs/1509.02971)
+
+# How to run
+python ac_pendulum.py
+
+It shows animation every 5 episodes.
+It reaches desired behaviour after around 40-60 episodes.
+<img src="animation.gif">
---
+++ DDPG_PYTORCH/model.py
@@ -0,0 +1,140 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def fanin_init(size, fanin=None):
+    fanin = fanin or size[0]
+    v = 1. / np.sqrt(fanin)
+    return torch.Tensor(size).uniform_(-v, v)
+
+
+class Actor(nn.Module):
+    def __init__(self, nb_states, nb_actions, hidden1=400, hidden2=300, init_w=3e-3):
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(nb_states, hidden1)
+        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.fc3 = nn.Linear(hidden2, nb_actions)
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+        self.init_weights(init_w)
+
+    def init_weights(self, init_w):
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3.weight.data.uniform_(-init_w, init_w)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        out = self.tanh(out)
+        return out
+
+
+class Critic(nn.Module):
+    def __init__(self, nb_states, nb_actions, hidden1=400, hidden2=300, init_w=3e-3):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(nb_states, hidden1)
+        self.fc2 = nn.Linear(hidden1 + nb_actions, hidden2)
+        self.fc3 = nn.Linear(hidden2, 1)
+        self.relu = nn.ReLU()
+        self.init_weights(init_w)
+
+    def init_weights(self, init_w):
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3.weight.data.uniform_(-init_w, init_w)
+
+    def forward(self, xs):
+        x, a = xs
+        out = self.fc1(x)
+        out = self.relu(out)
+        # debug()
+        out = self.fc2(torch.cat([out, a], 1))
+        out = self.relu(out)
+        out = self.fc3(out)
+        return out
+
+
+class UAActor(nn.Module):
+    def __init__(self, nb_states, nb_actions, dropout_n=3, dropout_p=0.2, hidden1=400, hidden2=300, init_w=3e-3):
+        super(UAActor, self).__init__()
+        self.fc1 = nn.Linear(nb_states, hidden1)
+        self.fc1_dropout = nn.Dropout(p=dropout_p)
+        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.fc2_dropout = nn.Dropout(p=dropout_p)
+        self.fc3 = nn.Linear(hidden2, nb_actions)
+        self.fc3_dropout = nn.Dropout(p=dropout_p)
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+        self.init_weights(init_w)
+        self.dropout_n = dropout_n
+
+    def init_weights(self, init_w):
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3.weight.data.uniform_(-init_w, init_w)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        out = self.tanh(out)
+        return out
+
+    def forward_with_dropout(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc1_dropout(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc2_dropout(out)
+        out = self.fc3(out)
+        out = self.tanh(out)
+        out = self.fc3_dropout(out)
+        return out
+
+
+class UACritic(nn.Module):
+    def __init__(self, nb_states, nb_actions, dropout_n=3, dropout_p=0.2, hidden1=400, hidden2=300, init_w=3e-3):
+        super(UACritic, self).__init__()
+        self.fc1 = nn.Linear(nb_states, hidden1)
+        self.fc1_dropout = nn.Dropout(p=dropout_p)
+        self.fc2 = nn.Linear(hidden1 + nb_actions, hidden2)
+        self.fc2_dropout = nn.Dropout(p=dropout_p)
+        self.fc3 = nn.Linear(hidden2, 1)
+        self.relu = nn.ReLU()
+        self.init_weights(init_w)
+        self.dropout_n = dropout_n
+
+    def init_weights(self, init_w):
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3.weight.data.uniform_(-init_w, init_w)
+
+    def forward(self, xs):
+        x, a = xs
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(torch.cat([out, a[:-1]], 1))
+        out = self.relu(out)
+        out = self.fc3(out)
+        return out
+
+    def forward_with_dropout(self, xs):
+        x, a = xs
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc1_dropout(out)
+        out = self.fc2(torch.cat([out, a[:-1]], 1))
+        out = self.relu(out)
+        out = self.fc2_dropout(out)
+        out = self.fc3(out)
+        return out
